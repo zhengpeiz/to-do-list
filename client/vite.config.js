08d26b5f3f5f49cf77d66d3151b7a87ba44e4329
zhengpeiz <zhengpeiz@student.unimelb.edu.au>
2026-03-01 22:46:59 +1100

    made front end deployable

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/to-do-list/',
   plugins: [react()],
-  server: {
-    proxy: {'/api': 'http://localhost:3001'} 
-  }
 })
 
